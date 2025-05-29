b8cd4759f001bca011a023c483f93575c4337840
serhiy-petryk <sergei_petrik@yahoo.com>
2025-05-30 00:46:46 +0300

    Update Program.cs

--- Pension2025/Pension2025/Program.cs
+++ Pension2025/Pension2025/Program.cs
@@ -14,26 +14,6 @@ namespace Pension2025
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            /*Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            string rtfFilePath = @"E:\Quote\WebData\Minute\Polygon2003\Data\MP2003_20250524\MP2003_A_20250512.json"; // Path to your RTF file
-            string txtFilePath = rtfFilePath.Replace(".json", ".3.json"); // Path to save plain text
-            var s = File.ReadAllText(rtfFilePath);
-            File.WriteAllText(txtFilePath, s, Encoding.GetEncoding("windows-1251"));*/
-
-            /*if (File.Exists(rtfFilePath))
-            {
-                // Create a hidden RichTextBox
-                using (RichTextBox rtb = new RichTextBox())
-                {
-                    rtb.LoadFile(rtfFilePath, RichTextBoxStreamType.RichText);
-//                    File.WriteAllText(txtFilePath, rtb.Text, Encoding.GetEncoding("windows-1251"));
-                    File.WriteAllText(txtFilePath, rtb.Text.Replace("\n", Environment.NewLine), Encoding.GetEncoding(1251));
-//                    File.WriteAllText(filePath, content, Encoding.UTF8);
-                    Console.WriteLine($"Conversion complete. Plain text saved to: {txtFilePath}");
-                }
-            }*/
-
             Application.Run(new Form1());
         }
     }
